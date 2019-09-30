1e1536bd4522f3f1885e5aba25cee5db7de8db2e
Zongwei Zhou <zongwei.zhou@Zongweis-MacBook-Air.local>
2019-09-30 16:10:47 +0800

    Delete download dataset

--- download_dataset.sh
+++
@@ -1,9 +0,0 @@
-FILE=$1
-
-URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
-ZIP_FILE=./datasets/$FILE.zip
-TARGET_DIR=./datasets/$FILE/
-wget -N $URL -O $ZIP_FILE
-mkdir $TARGET_DIR
-unzip $ZIP_FILE -d ./datasets/
-rm $ZIP_FILE
